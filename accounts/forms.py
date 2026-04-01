8b640afc6f6ad393121971280ac8bd421764d9aa
SasmikaWicky <sasmikasasinidu@gmail.com>
2026-04-01 15:12:24 +0530

    remove field

--- accounts/forms.py
+++ accounts/forms.py
@@ -15,7 +15,7 @@ class CustomUserChangeForm(UserChangeForm):
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email', 'profile_picture')
+        fields = ('first_name', 'last_name', 'email')
         widgets = {
             'first_name': forms.TextInput(attrs={'class': 'form-control'}),
             'last_name': forms.TextInput(attrs={'class': 'form-control'}),
