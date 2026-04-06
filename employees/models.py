5b72475f3d93fc82e6875afb0eb7c2cc7f57a785
mithilapremarathna <mithilapremarathna2004@gmail.com>
2026-04-06 15:09:46 +0530

    Create employee database models

---
+++ employees/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.conf import settings
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Employee(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True, blank=True)
+    designation = models.CharField(max_length=100)
+    joining_date = models.DateField()
+    phone_number = models.CharField(max_length=20)
+    address = models.TextField(blank=True)
+    base_salary = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return f"{self.user.get_full_name()} - {self.designation}"
