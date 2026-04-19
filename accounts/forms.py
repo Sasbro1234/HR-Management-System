88db8a6da46cabcb0cfb4e67792980b0c85da676
Gihan Eranda De Silva <gihanerandadesilva@gmail.com>
2026-04-19 10:27:01 +0530

    Update forms.py
    fixing an error

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,21 +6,7 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'role', 'first_name', 'last_name', 'profile_picture')
-        widgets = {
-    'username': forms.TextInput(attrs={'class': 'form-control'}),
-    'email': forms.EmailInput(attrs={'class': 'form-control'}),
-    'role': forms.Select(attrs={'class': 'form-control'}),
-    'first_name': forms.TextInput(attrs={'class': 'form-control'}),
-    'last_name': forms.TextInput(attrs={'class': 'form-control'}),
-    'profile_picture': forms.FileInput(attrs={'class': 'form-control'}),
-}
-    def clean_email(self):
-    email = self.cleaned_data.get('email')
-    if User.objects.filter(email=email).exists():
-        raise forms.ValidationError("Email already exists")
-    return email
- 
-
+        
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = User
